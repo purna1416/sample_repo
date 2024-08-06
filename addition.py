b41889ca4bb96560ce06057c2f0c7b9db70f6d5b
purna1416 <150007172+purna1416@users.noreply.github.com>
2024-08-06 11:27:39 +0530

    Add Addition file

---
+++ addition.py
@@ -0,0 +1,16 @@
+a=10
+b=3
+c=a+b
+d=a-b
+e=a*b
+f=a/b
+g=a%b
+h=a//b
+i=a**b
+print('addition of',a,'and',b,'is',c)
+print('subtraction  of',a,'and',b,'is',d)
+print('product  of',a,'and',b,'is',e)
+print('actual quotient of',a,'and',b,'is',f)
+print('remainder  of',a,'and',b,'is',g)
+print('floor quotient of',a,'and',b,'is',h)
+print('power of',a,'and',b,'is',i)
